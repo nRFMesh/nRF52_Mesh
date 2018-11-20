e52962f7fb818f7d06bfb0bb91bf54f6f5681774
wassfila <wassim.filali@gmail.com>
2018-11-20 15:27:22 +0100

    fixed action fields for zig cube

--- raspi/hue/hue.py
+++ raspi/hue/hue.py
@@ -18,8 +18,8 @@ import threading
 def aqara_cube(payload):
     log.debug("action => Aqara Cube 1")
     sensor = json.loads(payload)
-    if("event" in sensor):
-        if(sensor["event"] == "flip"):
+    if("action" in sensor):
+        if(sensor["action"] == "flip90"):
             lights["Stairs Up Left"].on = not lights["Stairs Up Left"].on
     return
 
@@ -154,41 +154,6 @@ def stairs_off_callback():
 g_stairs_up_light = 0.0
 g_stairs_down_light = 0.0
 
-def stairs_presence(name,payload):
-    global g_stairs_up_light
-    global g_stairs_down_light
-    #log.debug("stairs_presence : %s"%payload)
-    jval = json.loads(payload)
-    if("light" in jval):
-        log.debug("light => %f"%jval["light"])
-        if(name == "MotionLight 1"):
-            g_stairs_up_light = float(jval["light"])
-            log.debug("light => MotionLight Up : %f"%g_stairs_up_light)
-        if(name == "MotionLightHue"):
-            g_stairs_down_light = float(jval["light"])
-            log.debug("light => MotionLightHue: %f"%g_stairs_down_light)
-    if("presence" in jval):
-        log.debug("presence => %d"%jval["presence"])
-        if(jval["presence"]):
-            if(g_stairs_up_light < 2):
-                brightness = 254
-            elif(g_stairs_up_light < 12):
-                brightness = 128
-            else:
-                brightness = 10
-            if(name == "MotionLight 1"):
-                log.debug(f"presence => MotionLight Up - brightness:{brightness}")
-                b.set_light("Stairs Up Left", {'transitiontime' : 30, 'on' : True, 'bri' : brightness})
-                b.set_light("Stairs Down Right", {'transitiontime' : 10, 'on' : True, 'bri' : int(brightness/2)})
-                threading.Timer(60, stairs_off_callback).start()
-            if(name == "MotionLightHue"):
-                log.debug(f"presence => MotionLight Down - brightness:{brightness}")
-                b.set_light("Stairs Down Right", {'transitiontime' : 10, 'on' : True, 'bri' : brightness})
-                b.set_light("Stairs Up Left", {'transitiontime' : 30, 'on' : True, 'bri' : int(brightness/2)})
-                threading.Timer(60, stairs_off_callback).start()
-    return
-
-
 #new zig on mano
 def stairs_up_move(payload):
     global g_stairs_up_light
@@ -238,8 +203,6 @@ def stairs_down_move(payload):
             threading.Timer(60, stairs_off_callback).start()
     return
 
-
-
 def mqtt_on_message(client, userdata, msg):
     topic_parts = msg.topic.split('/')
     if(len(topic_parts) == 2 and topic_parts[0] == "zigbee2mqtt"):
@@ -262,24 +225,6 @@ def mqtt_on_message(client, userdata, msg):
             aqara_cube(msg.payload)
         elif(name == "night leds button"):
             night_leds_button()
-    elif(len(topic_parts) == 3):
-        modelid = topic_parts[1]
-        name = topic_parts[2]
-        log.debug(f"mqtt> name = {name} ; modelid = {modelid}")
-        if(modelid == "lumi.sensor_cube.aqgl01"):
-            log.debug("modelid : aqara_cube()")
-            aqara_cube(msg.payload)
-        elif(name == "lumi.remote.b1acn01 16"):
-            log.debug("name : aqara button 16")
-            night_leds_button()
-        elif(name == "LargeSingleSwitch"):
-            log.debug("name : LargeSingleSwitch")
-            dining_switch()
-        elif((modelid == "lumi.sensor_motion.aq2") or (modelid == "SML001") ):
-            log.debug("modelid : stairs_presence()")
-            stairs_presence(name,msg.payload)
-        else:
-            log.warning("WARNING : unused modelid")
     else:
         log.error("topic: "+msg.topic + "size not matching")
         
