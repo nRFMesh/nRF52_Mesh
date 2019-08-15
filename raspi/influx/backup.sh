18067081051ea2dfee8e72420639bc8c7165f5a5
wassfila <wassim.filali@gmail.com>
2019-08-15 18:56:51 +0200

    updated log and db name

--- raspi/influx/backup.sh
+++ raspi/influx/backup.sh
@@ -2,4 +2,4 @@
 echo "Backing up the influx Databse"
 influxd backup /home/pi/share/$(date +%F)_influx_backup
 influxd backup -database meshNodes -since 2017-12-01T00:00:00Z /home/pi/share/$(date +%F)_influx_backup
-influxd backup -database raspiStatus -since 2017-12-01T00:00:00Z /home/pi/share/$(date +%F)_influx_backup
+influxd backup -database meshNet -since 2017-12-01T00:00:00Z /home/pi/share/$(date +%F)_influx_backup
