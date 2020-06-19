cfcfd86289e13a5c50bb27baf862cd8435c60a50
wassfila <wass@nomail.com>
2020-06-19 18:57:38 +0200

    publish nodes file ony moved on top

--- nodes-server.sh
+++ nodes-server.sh
@@ -1,1 +1,1 @@
-docker run --name nodes-server -v /home/pi/nRF52_Mesh/www:/usr/share/nginx/html:ro -p 8080:80 -d --restart unless-stopped nginx
+docker run --name nodes-server -v /home/pi/nRF52_Mesh/www/nodes.json:/usr/share/nginx/html/nodes.json:ro -p 8080:80 -d --restart unless-stopped nginx
